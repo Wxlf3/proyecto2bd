9582b8fe074e28c04ab4036f39821fe8ff8fedfc
squesadav <sebas.quesada790@gmail.com>
2020-07-17 15:18:13 -0600

    me equivoque con el password de log xd

--- scripts/schemas/create_schemas.sql
+++ scripts/schemas/create_schemas.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA `log_ad` ;
 
 CREATE USER 'person_ad'@'localhost' IDENTIFIED WITH mysql_native_password BY 'person_ad';
 CREATE USER 'app_ad'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app_ad';
-CREATE USER 'log_ad'@'localhost' IDENTIFIED WITH mysql_native_password BY 'lod_ad';
+CREATE USER 'log_ad'@'localhost' IDENTIFIED WITH mysql_native_password BY 'log_ad';
 
 GRANT ALL ON `person_ad`.* TO 'person_ad'@'localhost';
 GRANT ALL ON `app_ad`.* TO 'app_ad'@'localhost';
