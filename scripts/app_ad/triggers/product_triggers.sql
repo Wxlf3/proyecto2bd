dc4304be00c21b1a7cd7f48be93765a1ff074408
squesadav <sebas.quesada790@gmail.com>
2020-07-23 00:06:57 -0600

    Trigger para price_changes en product

--- scripts/app_ad/triggers/product_triggers.sql
+++ scripts/app_ad/triggers/product_triggers.sql
@@ -12,9 +12,13 @@ DROP TRIGGER IF EXISTS `app_ad`.`product_BEFORE_UPDATE`;
 
 DELIMITER $$
 USE `app_ad`$$
-CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`product_BEFORE_UPDATE` BEFORE UPDATE ON `product` FOR EACH ROW
+CREATE DEFINER=`app_ad`@`localhost` TRIGGER `product_BEFORE_UPDATE` BEFORE UPDATE ON `product` FOR EACH ROW 
 BEGIN
-	SET NEW.last_change_user = current_user();
+    SET NEW.last_change_user = current_user();
     SET NEW.last_change_date = current_timestamp();
+    IF NOT (NEW.price <=> OLD.price) THEN
+		INSERT INTO `log_ad`.`price_changes`(id_product, date, prev_price, current_price)
+        VALUES (NEW.id, current_timestamp(), OLD.price, NEW.price);
+    END IF;
 END$$
 DELIMITER ;
