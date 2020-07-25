77ba18f45ff4dcedb3f4de420b7288fbd756ef09
squesadav <sebas.quesada790@gmail.com>
2020-07-24 21:08:02 -0600

    Agregados triggers en user_review y product_review

--- scripts/app_ad/triggers/productReview_triggers.sql
+++ scripts/app_ad/triggers/productReview_triggers.sql
@@ -8,6 +8,7 @@ BEGIN
 	SET NEW.creation_date = current_timestamp();
 END$$
 DELIMITER ;
+
 DROP TRIGGER IF EXISTS `app_ad`.`product_review_BEFORE_UPDATE`;
 
 DELIMITER $$
@@ -16,5 +17,32 @@ CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`product_review_BEFORE_UPDATE` BE
 BEGIN
 	SET NEW.last_change_user = current_user();
     SET NEW.last_change_date = current_timestamp();
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `app_ad`.`product_review_AFTER_INSERT`;
+
+DELIMITER $$
+USE `app_ad`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`product_review_AFTER_INSERT` AFTER INSERT ON `product_review` FOR EACH ROW
+BEGIN
+	UPDATE product
+    SET average_score = (SELECT AVG(score) FROM product_review WHERE id_product = NEW.id_product)
+    WHERE id = NEW.id_product;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `app_ad`.`product_review_AFTER_UPDATE`;
+
+DELIMITER $$
+USE `app_ad`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`product_review_AFTER_UPDATE` AFTER UPDATE ON `product_review` FOR EACH ROW
+BEGIN
+	UPDATE product
+    SET average_score = (SELECT AVG(score) FROM product_review WHERE id_product = NEW.id_product)
+    WHERE id = NEW.id_product;
+	UPDATE product
+    SET average_score = (SELECT AVG(score) FROM product_review WHERE id_product = OLD.id_product)
+    WHERE id = OLD.id_product;
 END$$
 DELIMITER ;
--- scripts/app_ad/triggers/userReview_triggers.sql
+++ scripts/app_ad/triggers/userReview_triggers.sql
@@ -18,3 +18,39 @@ BEGIN
     SET NEW.last_change_date = current_timestamp();
 END$$
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS `app_ad`.`user_review_AFTER_INSERT`;
+
+DELIMITER $$
+USE `app_ad`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`user_review_AFTER_INSERT` AFTER INSERT ON `user_review` FOR EACH ROW
+BEGIN
+	UPDATE user
+    SET average_score_buyer = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = NEW.username_receiver AND id_type_review = 1), 0)
+    WHERE username = NEW.username_receiver;
+	UPDATE user
+    SET average_score_seller = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = NEW.username_receiver AND id_type_review = 2), 0)
+    WHERE username = NEW.username_receiver;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `app_ad`.`user_review_AFTER_UPDATE`;
+
+DELIMITER $$
+USE `app_ad`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `app_ad`.`user_review_AFTER_UPDATE` AFTER UPDATE ON `user_review` FOR EACH ROW
+BEGIN
+	UPDATE user
+    SET average_score_buyer = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = NEW.username_receiver AND id_type_review = 1), 0)
+    WHERE username = NEW.username_receiver;
+	UPDATE user
+    SET average_score_seller = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = NEW.username_receiver AND id_type_review = 2), 0)
+    WHERE username = NEW.username_receiver;
+	UPDATE user
+    SET average_score_buyer = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = OLD.username_receiver AND id_type_review = 1), 0)
+    WHERE username = OLD.username_receiver;
+	UPDATE user
+    SET average_score_seller = IFNULL((SELECT AVG(score) FROM user_review WHERE username_receiver = OLD.username_receiver AND id_type_review = 2), 0)
+    WHERE username = OLD.username_receiver;
+END$$
+DELIMITER ;
