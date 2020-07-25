92f0294a7ee3211272e4e97d285c5cd59087336a
Sebasqc23 <squesadac@gmail.com>
2020-07-25 15:35:24 -0600

    Changes

--- scripts/GUI/Proyecto2/src/Connection/ConnectDB.java
+++ scripts/GUI/Proyecto2/src/Connection/ConnectDB.java
@@ -14,14 +14,14 @@ public class ConnectDB {
    
     private static Connection con;
     private static final String driver="com.mysql.jdbc.Driver";
-    private static final String user="root";
-    private static final String pass="";
+    private static final String user="person_ad";
+    private static final String pass="person_ad";
     private static final String url="jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
     
     public void conector() {
         con=null;
         try{
-            con= (Connection) DriverManager.getConnection(url, user, pass);
+            con=  DriverManager.getConnection(url, user, pass);
             if (con!=null){
                 System.out.println("Conexion establecida");
             }
@@ -29,18 +29,5 @@ public class ConnectDB {
         catch (SQLException e){
             System.out.println("Error de conexion" + e);
         }
-    }
-    
-    public static void insertUser(user pUser) throws SQLException 
-    {        
-        String host = "jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
-        String uName = "person_ad";
-        String uPass = "person_add";
-        
-        Connection con = DriverManager.getConnection(host, uName, uPass);
-        CallableStatement stmnt = (CallableStatement) con.prepareCall("{ call adminUser.createUser(?,?,?,?) } ");
-        
-        stmnt.execute();
-    }
-    
+    }    
 }
