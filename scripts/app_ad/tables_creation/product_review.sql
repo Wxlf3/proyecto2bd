95cca0e2682c945f5fd1bd2e12c33fd5515b4a03
squesadav <sebas.quesada790@gmail.com>
2020-07-24 21:07:39 -0600

    Agregada column id_product en product_review

--- scripts/app_ad/tables_creation/product_review.sql
+++ scripts/app_ad/tables_creation/product_review.sql
@@ -5,11 +5,14 @@ CREATE TABLE `product_review` (
   `score` decimal(4,2) NULL,
   `comment` varchar(45) NULL,
   `username_writer` varchar(45) NOT NULL,
+  `id_product` int NOT NULL,
   `creation_date` datetime NULL,
   `creation_user` varchar(45) NULL,
   `last_change_date` datetime NULL,
   `last_change_user` varchar(45) NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_productReview_user_idx` (`username_writer` ASC) VISIBLE,
-  CONSTRAINT `fk_productReview_user` FOREIGN KEY (`username_writer`) REFERENCES `user` (`username`)
+  INDEX `fk_productReview_idProduct_idx` (`id_product` ASC) VISIBLE,
+  CONSTRAINT `fk_productReview_user` FOREIGN KEY (`username_writer`) REFERENCES `user` (`username`),
+  CONSTRAINT `fk_productReview_idProduct` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`)
 )
