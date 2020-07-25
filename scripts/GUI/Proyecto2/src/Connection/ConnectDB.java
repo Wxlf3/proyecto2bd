b9ba2f38598d5c6ba94e1929b8bddaf62eecd965
Sebasqc23 <squesadac@gmail.com>
2020-07-25 16:42:50 -0600

    f

--- scripts/GUI/Proyecto2/src/Connection/ConnectDB.java
+++ scripts/GUI/Proyecto2/src/Connection/ConnectDB.java
@@ -1,7 +1,7 @@
 package Connection;
 
 import BL.user;
-import com.mysql.cj.jdbc.CallableStatement;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,66 +14,27 @@ public class ConnectDB {
    
     private static Connection con;
     private static final String driver="com.mysql.jdbc.Driver";
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private static final String user="app_ad";
-    private static final String pass="app_ad";
-    private static final String url="jdbc:mysql://127.0.0.1:3306/app_ad?user=app_ad";
-=======
-    private static final String user="root";
-    private static final String pass="";
-    private static final String url="jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
->>>>>>> parent of 92f0294... Changes
-=======
-    private static final String user="person_ad";
-    private static final String pass="person_ad";
-    private static final String url="jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
->>>>>>> parent of f685f8d... Changes
+    private static final String user_app="app_ad";
+    private static final String pass_app="app_ad";
+    private static final String url_app="jdbc:mysql://127.0.0.1:3306/app_ad?user=app_ad";
+    private static final String user_person="person_ad";
+    private static final String pass_person="person_ad";
+    private static final String url_person="jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
     
-    public void conector() {
-        con=null;
-        try{
-            con= (Connection) DriverManager.getConnection(url, user, pass);
-            if (con!=null){
-                System.out.println("Conexion establecida");
-            }
-        }
-        catch (SQLException e){
-            System.out.println("Error de conexion" + e);
-        }
-<<<<<<< HEAD
-    }
-    
-<<<<<<< HEAD
     public void insertDeliveryType(String pDescription) {
         con=null;
+        
         try{
             con=  DriverManager.getConnection(url, user, pass);
             CallableStatement stmt = con.prepareCall("{call insert_delivery_type(?)}");
             stmt.setString(1, pDescription);
-            if (stmt!=null){
+            stmt.execute();
+            if (con!=null){
                 System.out.println("Conexion establecida");
             }
         }
-        catch (SQLException e){
+        catch (Exception e){
             System.out.println("Error de conexion" + e);
         }
     }
-=======
-    public static void insertUser(user pUser) throws SQLException 
-    {        
-        String host = "jdbc:mysql://127.0.0.1:3306/person_ad?user=person_ad";
-        String uName = "person_ad";
-        String uPass = "person_add";
-        
-        Connection con = DriverManager.getConnection(host, uName, uPass);
-        CallableStatement stmnt = (CallableStatement) con.prepareCall("{ call adminUser.createUser(?,?,?,?) } ");
-        
-        stmnt.execute();
-    }
-    
->>>>>>> parent of 92f0294... Changes
-=======
-    }    
->>>>>>> parent of f685f8d... Changes
 }
