243b1f21ba3338e1939e729fae535dbfca77ea58
Sebasqc23 <squesadac@gmail.com>
2020-07-29 21:48:58 -0600

    Update person

--- scripts/delete this/6person.sql
+++ scripts/delete this/6person.sql
@@ -1,21 +1,15 @@
 DELETE FROM person;
 
-call insert_person("147475047", "Diana", "", "Reyes","dreyes@gmail.com","66825848", 23/11/1967,"",2,5,"dReyes");
-call insert_person("947463639", "Rigoberto", "Enrique", "Quesada","rQuesada@gmail.com","86827888", 10/11/1987,"",3,8,"rQuesada");
-
-
-
-
-
-
-call insert_user("sCalderón", "1288", 5);
-call insert_user("tCalvo", "1288936", 4);
-call insert_user("dCalvo", "97336", 2);
-call insert_user("mPacheco", "973647", 3);
-call insert_user("lChacon", "1234", 2);
-call insert_user("hMendoza", "123cuatro", 3);
-call insert_user("jPerez", "1288", 5);
-call insert_user("gGuzman", "1288936", 4);
-call insert_user("lNuñez", "97336", 2);
-call insert_user("fRojas", "973647", 3);
-call insert_user("sRojas", "738493", 5);
+call insert_person("147475047", "Diana", "", "Reyes","dreyes@gmail.com","66825848", STR_TO_DATE('23,11,1967','%d,%m,%Y'),"",2,5,"dReyes");
+call insert_person("747463639", "Rigoberto", "Enrique", "Quesada","rQuesada@gmail.com","86827888", STR_TO_DATE('10,11,1987','%d,%m,%Y'),"",1,8,"rQuesada");
+call insert_person("576893663", "Susan", "Daniela", "Calderon","sCalderon@gmail.com","86459248", STR_TO_DATE('05,05,1996','%d,%m,%Y'),"",3,10,"sCalderon");
+call insert_person("346824946", "Tatiana", "Karol", "Calvo","tCalvo@gmail.com","65888948", STR_TO_DATE('01,01,1977','%d,%m,%Y'),"",2,14,"tCalvo");
+call insert_person("285638293", "Daniela", "Jazmin", "Calvo","dCalvo@gmail.com","86526438", STR_TO_DATE('20,06,1995','%d,%m,%Y'),"",3,1,"dCalvo");
+call insert_person("476475047", "Mariana", "Carolina", "Pacheco","mPacheco@gmail.com","66567288", STR_TO_DATE('05,02,2001','%d,%m,%Y'),"",2,11,"mPacheco");
+call insert_person("643465047", "Luis", "", "Chacon","lchacon@gmail.com","668255309", STR_TO_DATE('09,10,2000','%d,%m,%Y'),"",1,15,"lChacon");
+call insert_person("149735047", "Hugo", "Gabriel", "Mendoza","hMendoza@gmail.com","84635848", STR_TO_DATE('20,06,2002','%d,%m,%Y'),"",3,3,"hMendoza");
+call insert_person("247543047", "Juan", "Gabriel", "Perez","jPerez@gmail.com","85630848", STR_TO_DATE('16,09,2004','%d,%m,%Y'),"",1,20,"jPerez");
+call insert_person("548755047", "Gabriel", "Luis", "Guzman","gGuzman@gmail.com","66729048", STR_TO_DATE('18,07,1989','%d,%m,%Y'),"",1,16,"gGuzman");
+call insert_person("382847395", "Luis", "Bastian", "Nuñez","lnuñez@gmail.com","78465848", STR_TO_DATE('18,05,1995','%d,%m,%Y'),"",1,14,"lNuñez");
+call insert_person("487465047", "Fabian", "Jesus", "Rojas","frojas@gmail.com","56804848", STR_TO_DATE('15,07,1993','%d,%m,%Y'),"",3,13,"fRojas");
+call insert_person("686705825", "Susie", "", "Rojas","srojas@gmail.com","66560628", STR_TO_DATE('11,12,1985','%d,%m,%Y'),"",2,2,"sRojas");
