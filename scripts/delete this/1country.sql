7774d2c775cdb36c35b4e3cead76060f38ad8499
squesadav <sebas.quesada790@gmail.com>
2020-07-30 06:30:26 -0600

    bye

--- scripts/delete this/1country.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM country;
-ALTER TABLE country AUTO_INCREMENT = 1;
-
-INSERT INTO `person_ad`.`country` (`name`) VALUES ('Costa Rica');
-INSERT INTO `person_ad`.`country` (`name`) VALUES ('Estados Unidos');
-INSERT INTO `person_ad`.`country` (`name`) VALUES ('México');
-INSERT INTO `person_ad`.`country` (`name`) VALUES ('Colombia');
