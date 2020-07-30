f8abc0ed9cc361debc0cf941070c6f0584390a91
Sebasqc23 <squesadac@gmail.com>
2020-07-30 00:30:52 -0600

    Changes

---
+++ scripts/delete this/9product.sql
@@ -0,0 +1,5 @@
+DELETE FROM product;
+
+call insert_product(16.99,"Old lamp","Was of my dead grandma",1,0,9,"tCalvo",3);
+
+call insert_product(23,"Phone","Used in Yugoslavia war of 1923",5,0,6,"jPerez",7);
