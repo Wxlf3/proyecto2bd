e51259578f2d78ff95f8170965559987661192b5
Sebasqc23 <squesadac@gmail.com>
2020-07-29 21:28:57 -0600

    Changes

---
+++ scripts/delete this/5user_type.sql
@@ -0,0 +1,7 @@
+DELETE FROM user_type;
+
+call insert_userType("admin");
+call insert_userType("normal");
+call insert_userType("q&a");
+call insert_userType("IT");
+call insert_userType("human resources");
---
+++ scripts/delete this/6person.sql
@@ -0,0 +1,21 @@
+DELETE FROM person;
+
+call insert_person("147475047", "Diana", "", "Reyes","dreyes@gmail.com","66825848", 23/11/1967,"",2,5,"dReyes");
+call insert_person("947463639", "Rigoberto", "Enrique", "Quesada","rQuesada@gmail.com","86827888", 10/11/1987,"",3,8,"rQuesada");
+
+
+
+
+
+
+call insert_user("sCalderón", "1288", 5);
+call insert_user("tCalvo", "1288936", 4);
+call insert_user("dCalvo", "97336", 2);
+call insert_user("mPacheco", "973647", 3);
+call insert_user("lChacon", "1234", 2);
+call insert_user("hMendoza", "123cuatro", 3);
+call insert_user("jPerez", "1288", 5);
+call insert_user("gGuzman", "1288936", 4);
+call insert_user("lNuñez", "97336", 2);
+call insert_user("fRojas", "973647", 3);
+call insert_user("sRojas", "738493", 5);
